b6736eb4d6308747a32c3ab28b8770f4b0251fce
Alexeev Alexander <work.alexeevs@gmail.com>
2023-02-02 10:15:22 +0200

    user seed hash password

--- src/database/seeder/1675325049106-seed.ts
+++ src/database/seeder/1675325049106-seed.ts
@@ -1,12 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { UserEntity } from '../../user/user.entity';
+import * as bcrypt from 'bcrypt';
 
 export class seed1675325049106 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.manager.insert(UserEntity, [
       {
         email: 'test@mail.ru',
-        password: '1234',
+        password: await bcrypt.hash('1234', 10),
       },
     ]);
   }
